09a0f17821c69ca31b985d9f365cac55eddf34df
dawdawdwadadawd <44469756+dawdawdwadadawd@users.noreply.github.com>
2018-10-27 16:29:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -235,14 +235,14 @@ client.on('message',async message => {
   if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
   if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
   message.channel.send('✅| **تم عمل الروم بنجاح**');
-  message.guild.createChannel(`Voice  ⌜ ${message.guild.members.filter(m => m.voiceChannel).size} ⌟  Online` , 'voice').then(c => {
+  message.guild.createChannel(`Voice  ⎾ ${message.guild.members.filter(m => m.voiceChannel).size} ⏌  Online` , 'voice').then(c => {
     console.log(`Voice online channel setup for guild: \n ${message.guild.name}`);
     c.overwritePermissions(message.guild.id, {
       CONNECT: false,
       SPEAK: false
     });
     setInterval(() => {
-      c.setName(`Voice  ⌜ ${message.guild.members.filter(m => m.voiceChannel).size} ⌟  Online`)
+      c.setName(`Voice  ⎾ ${message.guild.members.filter(m => m.voiceChannel).size} ⏌  Online`)
     },1000);
   });
   }
